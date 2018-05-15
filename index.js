e35fc859d04f77e25c64aa6467ccb917366436ed
Luke Bertram <lukebertram@gmail.com>
2018-05-14 17:07:03 -0700

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+module.exports = Phrase;
 //Adds 'reverse' to all strings.
 // Reverses a string
 String.prototype.reverse = function() {
@@ -22,11 +22,16 @@ function Phrase(content) {
 
   //Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.processor(this.letters());
   }
   // Returns true for a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
+  }
+
+  // Returns the letters in the content without punctuation, whitespace
+  this.letters = function letters() {
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
   }
 
   // Makes the phrase LOUDER.
